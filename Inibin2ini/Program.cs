3e44dfcfce0b47ea3d7639fd975edacb50762962
0x-C3 <62750690+0x-C3@users.noreply.github.com>
2020-08-02 00:08:00 +0800

    Amazing!

--- Inibin2ini/Program.cs
+++ Inibin2ini/Program.cs
@@ -30,60 +30,35 @@ namespace Inibin_To_ini
                         sets.Add("default", new Dictionary<string, string>());
                         sets.Add("System", new Dictionary<string, string>());
                         //Load Hashtable
-                        string[] keys = System.Text.Encoding.Default.GetString(Resources.inibin2).Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                        List<string> keys = System.Text.Encoding.Default.GetString(Resources.inibin2).Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries).ToList();
                         // head
                         Dictionary<uint, string> keyHead = new Dictionary<uint, string>();
                         Dictionary<uint, string> hashMap = new Dictionary<uint, string>();
-                        foreach (var head in sets.Keys)
-                        {
+                        foreach (var section in sets.Keys)
                             foreach (var key in keys)
                             {
-                                uint hash = SectionHash(head, key);
+                                uint hash = SectionHash(section, key);
                                 if (!keyHead.ContainsKey(hash))
-                                {
-                                    keyHead.Add(hash, head);
-                                }
+                                    keyHead.Add(hash, section);
                                 if (!hashMap.ContainsKey(hash))
-                                {
                                     hashMap.Add(hash, key);
-                                }
                             }
-                        }
                         sets.Add("Unknown", new Dictionary<string, string>());
-                        foreach (var flag in ini.Sets.Keys)
-                        {
-                            foreach (var i in ini.Sets[flag].Properties)
-                            {
-                                string headstr = keyHead.ContainsKey(i.Key) ? keyHead[i.Key] : "Unknown";
-                                string value = string.Empty;
-                                if (flag == InibinFlags.FixedPointFloatListVec2 || flag == InibinFlags.FixedPointFloatListVec3 || flag == InibinFlags.FixedPointFloatListVec4 || flag == InibinFlags.Float32ListVec2 || flag == InibinFlags.Float32ListVec3 || flag == InibinFlags.Float32ListVec4)
-                                {
-                                    value = "[";
-                                    foreach (var c in (Array)i.Value)
-                                    {
-                                        value += c.ToString() + ",";
-                                    }
-                                    value = value.Substring(0, value.Length - 1) + "]";
-                                }
-                                else
-                                {
-                                    value = i.Value.ToString();
-                                }
-                                sets[headstr].Add(hashMap.TryGetValue(i.Key, out string result) ? result : i.Key.ToString(), value);
-                            }
-                        }
-                        List<string> lines = new List<string>();
-                        foreach (var item in sets)
-                        {
-                            if (item.Value.Count != 0)
-                            {
-                                lines.Add($"[{item.Key}]");
-                                foreach (var keyValuePair in item.Value)
-                                {
-                                    lines.Add($"{keyValuePair.Key}={keyValuePair.Value}");
-                                }
-                            }
-                        }
+                        var pairs = from InibinFlags flag in ini.Sets.Keys
+                                 from KeyValuePair<uint, object> pair in ini.Sets[flag].Properties
+                                 let sectionstr = from KeyValuePair<uint, string> sections in keyHead where sections.Key == pair.Key select sections.Value
+                                 let property = from KeyValuePair<uint, string> hashes in hashMap where hashes.Key == pair.Key select hashes.Value
+                                 let value = (flag == InibinFlags.FixedPointFloatListVec2 || flag == InibinFlags.FixedPointFloatListVec3 || flag == InibinFlags.FixedPointFloatListVec4 || flag == InibinFlags.Float32ListVec2 || flag
+                                 == InibinFlags.Float32ListVec3 || flag == InibinFlags.Float32ListVec4) ?
+                                     $"[{ string.Join("", from object z in (Array)pair.Value select $"{z},").TrimEnd(',')}]" : pair.Value.ToString()
+                                 select new Tuple<string, string, string>(sectionstr.Count() == 0 ? "Unknown" : sectionstr.First(), property.Count() == 0 ? pair.Key.ToString() : property.First(), value);
+                        foreach (var x in pairs)
+                            sets[x.Item1].Add(x.Item2, x.Item3);
+
+                        var lines = from KeyValuePair<string, Dictionary<string, string>> i in sets
+                                 where i.Value.Count != 0
+                                 from KeyValuePair<string, string> keyValuePair in i.Value
+                                 select ((i.Value.Keys.First() == keyValuePair.Key && i.Value[keyValuePair.Key] == keyValuePair.Value) ? $"[{i.Key}]\r\n" : string.Empty) + $"{keyValuePair.Key}={keyValuePair.Value}";
                         if (args.Length > 1)
                         {
                             var path = string.Empty;
@@ -119,9 +94,9 @@ namespace Inibin_To_ini
                 }
             }
         }
-        public static UInt32 SectionHash(string section, string property)
+        public static uint SectionHash(string section, string property)
         {
-            UInt32 hash = 0;
+            uint hash = 0;
             section = section.ToLower();
             property = property.ToLower();
             for (int i = 0; i < section.Length; i++)
